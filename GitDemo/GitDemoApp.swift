98c8dc69f407c97e7385e0fbe8d98a2d7ab31dd7
Jannis <40733092+jannis6023@users.noreply.github.com>
2024-11-14 09:03:47 +0100

    Initial Commit

---
+++ GitDemo/GitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitDemoApp.swift
+//  GitDemo
+//
+//  Created by Jannis Gollwitzer on 14.11.24.
+//
+
+import SwiftUI
+
+@main
+struct GitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
